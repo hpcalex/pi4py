45a8fa44914bd45951d6ea51bfecd4db41e32033
Youssef Eldakar <youssefeldakar@gmail.com>
2019-01-21 15:52:59 +0200

    Make pi4py-g.sh consistent with pi4py-p.sh

--- pi4py-g.sh
+++ pi4py-g.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#SBATCH --job-name=pi4py_cuda
+#SBATCH --job-name=pi4py-g
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=00:15:00
 
-nvprof python pi4py-g.py "$@"
+nvprof ./pi4py-g.py "$@"
